b69faad5c1eb9c12dbd9e85930dceabc22f7c95d
Araceli <andi.mondaca2021@gmail.com>
2024-09-19 14:55:46 -0300

    Agregando cambios en accionBuscarPersona.php

--- view/action/accionBuscarPersona.php
+++ view/action/accionBuscarPersona.php
@@ -1,23 +1,24 @@
 <?php
-
-include_once('../../util/funciones.php');
+include_once("../../util/configuracion.php");
 include_once('../../controller/ABM.php');
 
 $datos = dataSubmmited(); 
-$dni = isset($datos['NroDni']) ? trim($datos['NroDni']) : null;
+$dni = isset($datos['nroDni']) ? trim($datos['nroDni']) : null;
 $mensaje = ""; 
 
 if ($dni) {
     $control = new ABM();
-    $persona = $control->buscarPersona($dni); 
+    
+    $persona = $control->buscarPersona(['nroDni' => $dni]); 
+    
     if ($persona) {
         $mensaje .= "DNI: " . $persona->getNroDni() . "<br>";
         $mensaje .= "Apellido: " . $persona->getApellido() . "<br>";
         $mensaje .= "Nombre: " . $persona->getNombre() . "<br>";
-        $mensaje .= "Fecha de Nacimiento: " . $persona->getFechaNac() . "<br>";
+        $mensaje .= "Fecha de Nacimiento: " . $persona->getFechaNacimiento() . "<br>";
         $mensaje .= "Telefono: " . $persona->getTelefono() . "<br>";
         $mensaje .= "Domicilio: " . $persona->getDomicilio() . "<br>";
-        $mensaje .= "<a href='ListarPersonas.php'>Volver a Listar Todas las Personas</a><br>"; 
+        
     } else {
         $mensaje = "No se encontró la persona con el DNI " . htmlspecialchars($dni) . ".";
     }
@@ -29,13 +30,23 @@ if ($dni) {
 <!DOCTYPE html>
 <html>
 <head>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
     <title>Resultado de Búsqueda</title>
     <meta charset="UTF-8">
 </head>
 <body>
-    <h3>Información de la Persona</h3>
-    <br><a href="/PWD_TP4/view/BuscarPersona.php">Volver</a><br>
-    <?php echo $mensaje; ?>
+    <div class="container">
+        <h3 class="center-align">Información de la Persona</h3>
+        
+        <div class="card-panel grey lighten-3">
+            <p><?php echo $mensaje; ?></p>
+        </div>
+
+        <a href="../BuscarPersona.php" class="waves-effect waves-light btn-small">Volver</a>
+    </div>
+
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
